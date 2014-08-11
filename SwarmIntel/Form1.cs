99541a5f197a561023b4bf47e79bb560538ef443
Nihilkri <nehilkori@gmail.com>
2014-08-10 20:23:18 -0500

    Robots seek out each other, Red averages centroid and override

--- SwarmIntel/Form1.cs
+++ SwarmIntel/Form1.cs
@@ -12,9 +12,12 @@ namespace SwarmIntel {
 	public partial class World : Form {
 		#region Variables
 		const double pi = Math.PI, pi2 = 2 * Math.PI;
-		Graphics gb, gf; Bitmap gi; int fx, fy, fx2, fy2;
-		Random rnd = new Random(); Timer tim = new Timer();
-		List<Robot> bots = new List<Robot>(); bool active = true;
+		public static Random rnd = new Random(); 
+		public static int fx, fy, fx2, fy2;
+		private static List<Robot> bots = new List<Robot>();
+		private static Bitmap gi; Graphics gb, gf;
+		Timer tim = new Timer(); DateTime st; TimeSpan ft;
+		bool active = true;
 
 		#endregion Variables
 		#region Events
@@ -31,7 +34,7 @@ namespace SwarmIntel {
 			bots.Add(new Robot(bots.Count(), Color.Blue, rnd.NextDouble() * fx, rnd.NextDouble() * fy));
 			bots.Add(new Robot(bots.Count(), Color.Purple, rnd.NextDouble() * fx, rnd.NextDouble() * fy));
 
-			tim.Interval = 1000 / 60; tim.Tick += tim_Tick;
+			tim.Interval = 1000 / 60; tim.Tick += tim_Tick; tim.Start();
 
 			Calc(); Draw();
 		}
@@ -39,7 +42,7 @@ namespace SwarmIntel {
 		private void Form1_Paint(object sender, PaintEventArgs e) { gf.DrawImage(gi, 0, 0); }
 
 		private void Form1_MouseClick(object sender, MouseEventArgs e) {
-			bots[0].dx = e.X; bots[0].dy = e.Y; Draw(); if(bots[0].Active()) active = true;
+			bots[0].ox = e.X; bots[0].oy = e.Y; Draw(); if(bots[0].Active()) active = true;
 
 		}
 
@@ -59,25 +62,37 @@ namespace SwarmIntel {
 		#endregion Events
 		#region Calc
 		void Calc() {
+			st = DateTime.Now;
+			gb.Clear(Color.Black);
+
+			active = false;
+			foreach(Robot b in bots) {
+				b.Calc(); active = active || b.Active();
+
+			}
 
 		}
 		#endregion Calc
 		#region Draw
 		void Draw() {
-			DateTime st = DateTime.Now; TimeSpan ft;
-			gb.Clear(Color.Black);
-			foreach(Robot q in bots) {
-				q.Draw(gb);
+			foreach(Robot b in bots) {
+				b.Draw(gb);
 				
 			}
+			if(!active) for(int q = 0 ; q < 5 ; q++) gb.DrawRectangle(Pens.Red, q, q, fx - 2 * q, fy - 2 * q);
 
-			DateTime sst = DateTime.Now; TimeSpan sft;
-			for(int b = 0 ; b < bots.Count ; b++) for(double q = 0 ; q < pi2 ; q += pi2 / 360.0) Scan(b, q, 250, 1);
-			sft = DateTime.Now - sst;
-			gb.DrawString(sft.TotalMilliseconds.ToString(), Font, Brushes.White, fx2, 0);
+			//DateTime sst = DateTime.Now; TimeSpan sft;
+			//for(int b = 0 ; b < bots.Count ; b++)
+			//	//for(double q = -pi2 / 8 ; q < pi2 / 8 ; q += 5.0 * pi2 / 360.0)
+			//	for(double q = 0 ; q < pi2 ; q += 5.0 * pi2 / 360.0)
+			//		Scan(b, q, 250, 16);
+			//sft = DateTime.Now - sst;
+			//gb.DrawString(sft.TotalMilliseconds.ToString() + "ms", Font, Brushes.White, fx2, 0);
+			//gb.DrawString((1000 / sft.TotalMilliseconds).ToString() + " FPS", Font, Brushes.White, fx2, 16);
 
 			ft = DateTime.Now - st;
-			gb.DrawString(ft.TotalMilliseconds.ToString(), Font, Brushes.White, 0, 0);
+			gb.DrawString(ft.TotalMilliseconds.ToString() + "ms", Font, Brushes.White, 0, 0);
+			gb.DrawString((1000 / ft.TotalMilliseconds).ToString() + " FPS", Font, Brushes.White, 0, 16);
 			gf.DrawImage(gi, 0, 0);
 		}
 		#endregion Draw
@@ -89,7 +104,7 @@ namespace SwarmIntel {
 		/// <param name="r">Radius of scan</param>
 		/// <param name="i">Intensity or resolution</param>
 		/// <returns></returns>
-		public double Scan(int n, double a, double r, double i) {
+		public static double Scan(int n, double a, double r, double i) {
 			a = (a + bots[n].T + pi2) % pi2 + pi2;
 			int x, y;
 			for(double q = bots[n].width ; q < r ; q += i) {
--- SwarmIntel/Robot.cs
+++ SwarmIntel/Robot.cs
@@ -8,10 +8,12 @@ using System.Threading.Tasks;
 namespace SwarmIntel {
 	class Robot {
 		#region Variables
+		const double pi = Math.PI, pi2 = 2 * Math.PI;
 		public int n;
 
 		private double x,  y,  t,  xx,  yy,  tt;
-		public double dx, dy, dt, dxx, dyy, dtt;
+		private double dx, dy, dt, dxx, dyy, dtt;
+		public double ox, oy, ot, oxx, oyy, ott;
 		public double X  { get { return x ; } }
 		public double Y  { get { return y ; } }
 		public double T  { get { return t ; } }
@@ -19,7 +21,7 @@ namespace SwarmIntel {
 		public double YY { get { return yy; } }
 		public double TT { get { return tt; } }
 
-		public double width = 16.0, depth = 10.0;
+		public double width = 16.0, depth = 16.0;
 		public Point fl, fr, bl, br;
 		public Rectangle rect;
 
@@ -27,8 +29,9 @@ namespace SwarmIntel {
 		#endregion Variables
 
 		public Robot(int nn, Color nc, double nx, double ny, double nt = 0.0) {
+			ox = oy = ot = oxx = oyy = ott = -1;
 			x = dx = nx; y = dy = ny; t = dt = nt; xx = yy = tt = dxx = dyy = dtt = 0;
-			c = nc; p = new Pen(c); b = new SolidBrush(c); n = nn; Calc();
+			c = nc; p = new Pen(c); b = new SolidBrush(c); n = nn; //Calc();
 		
 		}
 
@@ -43,11 +46,27 @@ namespace SwarmIntel {
 		}
 
 		public void Calc() {
+			double tx = 0, ty = 0, d = 0; int tc = 0;
+			for(double a = 0 ; a < pi2 ; a += 10.0 * pi2 / 360.0)
+				if((d = World.Scan(n, a, 500, 16)) > 0) { tc++; 
+					tx += (Math.Cos(a) * d); ty += (Math.Sin(a) * d); }
+			if(tc > 0) { dx = x + tx / tc; dy = y + ty / tc; }
+
+			dx = (ox > -1) ? (dx + ox) / 2 : dx;
+			dy = (oy > -1) ? (dy + oy) / 2 : dy;
+
+			xx = Math.Sign(dx - x); yy = Math.Sign(dy - y);
+
+			x += xx; y += yy;
+
+
+
 			fl = new Point((int)(x - width / 2.0), (int)(y - depth / 2.0));
 			fr = new Point((int)(x + width / 2.0), (int)(y - depth / 2.0));
 			bl = new Point((int)(x - width / 2.0), (int)(y + depth / 2.0));
 			br = new Point((int)(x + width / 2.0), (int)(y + depth / 2.0));
 			rect = new Rectangle(fl.X, fl.Y, (int)width, (int)depth);
+
 
 		}
 		public void Draw(Graphics gb) {
@@ -59,6 +78,9 @@ namespace SwarmIntel {
 
 			gb.DrawLine(p, (int)(dx - width / 2), (int)(dy), (int)(dx + width / 2), (int)(dy));
 			gb.DrawLine(p, (int)(dx), (int)(dy - depth / 2), (int)(dx), (int)(dy + depth / 2));
+			gb.DrawLine(p, (int)(ox - width / 2), (int)(oy), (int)(ox + width / 2), (int)(oy));
+			gb.DrawLine(p, (int)(ox), (int)(oy - depth / 2), (int)(ox), (int)(oy + depth / 2));
+			gb.DrawEllipse(p, (int)(ox - width / 2), (int)(oy - depth / 2), (int)width, (int)depth);
 
 
 		}
